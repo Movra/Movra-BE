0264b1f4e86261c98e5e0d52535f9bbcaae6ddc5
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-02-24 17:10:50 +0900

    first commit

---
+++ src/main/java/com/example/morva/MorvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.morva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/morva/MorvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.morva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
