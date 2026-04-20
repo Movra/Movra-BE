29e3065f591fbc86c24b32b88ef389b69474b255
hyohyeon08 <hyohyeonkim08@gmail.com>
2026-04-20 14:16:16 +0900

    refactor: message 오류 해결

--- src/main/java/com/example/movra/bc/accountability/accountability_relation/application/service/dto/request/JoinAccountabilityRelationRequest.java
+++ src/main/java/com/example/movra/bc/accountability/accountability_relation/application/service/dto/request/JoinAccountabilityRelationRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 public record JoinAccountabilityRelationRequest(
 
         @NotBlank(message = "inviteCode는 필수입니다.")
-        @Size(max = 10, message = "account ID를 10 사이로 입력해주세요.")
+        @Size(max = 10, message = "inviteCode를 10 사이로 입력해주세요.")
         String inviteCode
 ) {
 }
